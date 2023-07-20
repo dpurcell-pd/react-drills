c589a0205a2c23aa2234f76c21441d1aada813ef
dpurcell <dpurcell.pd@gmail.com>
2023-07-20 10:01:20 -0400

    Object looping

---
+++ src/Objects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Objects() {
+
+    const zuko = {
+        firstName: "Zuko",
+        age: 3,
+        email: "abc@123.com"
+    };
+
+    return Object.keys(zuko).map((key, index) => {
+        return (
+            <h2>
+                {key} : {zuko[key]}
+            </h2>
+        )
+    });  
+}
+
+export default Objects
