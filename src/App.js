790d367214886be12f173cd7355766949b70d697
dpurcell <dpurcell.pd@gmail.com>
2023-07-20 10:33:56 -0400

    State and event handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import Arrays from './Arrays';
-import Objects from './Objects'
+import Arrays from "./Arrays";
+import Objects from "./Objects";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  }
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
       <h1>React Drills</h1>
-      {/*<Arrays />*/}
-      <Objects />
+      
+      {/* <Arrays /> */}
+      {/* <Objects /> */}
+      
+      <h1> {counter} </h1>
+      <button onClick = {handleIncrement}> + </button>
+      <button onClick = {() => {setCounter(counter - 1)}}> - </button>
+      <br />
+      <button onClick = {() => {setCounter(0)}}> RESET </button>
+    
     </div>
   );
 }
